8fb685932247468b997efcc91fc1a286ae341bc7
Dmitry Ulitin <dmitry.ulitin@gmail.com>
2018-08-05 11:19:41 +0300

    delete effect

--- frontend/src/app/accounts/accounts.effects.ts
+++ frontend/src/app/accounts/accounts.effects.ts
@@ -1,24 +1,38 @@
 import { Injectable } from "@angular/core";
 import { Router } from '@angular/router'
 import { Observable, of } from 'rxjs';
-import { switchMap, map } from 'rxjs/operators';
+import { switchMap, map, withLatestFrom, filter } from 'rxjs/operators';
 import { Actions, Effect, ofType } from '@ngrx/effects';
+import { Store } from '@ngrx/store';
+import { State } from '../app.reducers'
 import { BackendService } from '../backend.service'
 
 @Injectable()
 export class AccountsEffects {
-    constructor(private actions$: Actions<any>, private backend: BackendService, private router: Router) { };
+    constructor(private actions$: Actions<any>, private store: Store<State>, private backend: BackendService, private router: Router) { };
 
     @Effect() getAccounts$: Observable<any> = this.actions$.ofType('[accounts] query').pipe(
         switchMap(action => this.backend.getAccounts().pipe(
             map(data => { return { type: '[accounts] query success', payload: data }; })
         ))
     );
+
     @Effect() saveAccount$: Observable<any> = this.actions$.ofType('[account] save').pipe(
         switchMap(action => this.backend.saveAccount(action.payload).pipe(
             map(data => {
                 this.router.navigate(['/accounts']);
                 return { type: '[account] save success', payload: data };
+            })
+        ))
+    );
+
+    @Effect() deleteAccount$: Observable<any> = this.actions$.ofType('[account] delete').pipe(
+        withLatestFrom(this.store),
+        filter(([action, state]) => state.accounts.selected != null),
+        switchMap(([action, state]) => this.backend.deleteAccount(state.accounts.selected.id).pipe(
+            map(data => {
+                this.router.navigate(['/accounts']);
+                return { type: '[account] delete success' };
             })
         ))
     );
