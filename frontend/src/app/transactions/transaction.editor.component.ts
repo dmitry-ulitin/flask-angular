f9205ed3dff2af6b1c4e468e9347e3745b87de17
Dmitry Ulitin <dmitry.ulitin@gmail.com>
2018-09-24 23:59:12 +0300

    save progress

--- frontend/src/app/transactions/transaction.editor.component.ts
+++ frontend/src/app/transactions/transaction.editor.component.ts
@@ -3,7 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common'
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Observable} from 'rxjs';
-import { filter } from 'rxjs/operators'
+import { filter, tap } from 'rxjs/operators'
 import { Store } from '@ngrx/store';
 import { State } from '../app.reducers'
 import { Account } from '../models/account';
@@ -23,11 +23,19 @@ export class TransactionEditorComponent implements OnInit {
     this.form = this.fb.group({
       amount: ['', Validators.required],
       currency: ['', Validators.required],
-      opdate: [],
+      account: ['', Validators.required],
+      opdate: [new Date().toISOString().substr(0,10), Validators.required],
       details: []
     });
     this.store.select('transactions', 'selected').pipe(filter(t => t != null)).forEach(a => this.form.patchValue(a));
-    this.accounts$ = this.store.select('accounts', 'accounts');
+    this.accounts$ = this.store.select('accounts', 'accounts').pipe(tap(a => {
+      if (a.length && !this.form.controls.account.value) {
+        this.form.controls.account.setValue(a[0]);
+      }
+    }));
+    this.form.controls.account.valueChanges.forEach(e => {
+      this.form.controls.currency.setValue(e.currency)
+    });
   }
 
   onSubmit({ value, valid }) {
