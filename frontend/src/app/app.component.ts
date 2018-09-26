224e8e41a9465680fc3b40edab674dd42a8447af
Dmitry Ulitin <dmitry.ulitin@nexign-systems.com>
2018-09-26 17:31:57 +0300

    save progress

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -4,12 +4,12 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template: `
 <div class="d-flex flex-column h-100">
-  <nav class="navbar navbar-light bg-light navbar-expand-sm">
+  <nav class="navbar navbar-light bg-light">
     <div class="container">
       <span class="navbar-brand mb-0 h1">Swarmer</span>
-      <div class="navbar-nav">
-        <a class="nav-item nav-link" href="#" routerLink="/accounts" routerLinkActive="active">Accounts</a>
-        <a class="nav-item nav-link" href="#" routerLink="/transactions" routerLinkActive="active">Transactions</a>
+      <div class="d-flex flex-row">
+        <a class="nav-item nav-link" href="#" routerLink="/accounts" routerLinkActive="active"><i class="fas fa-database"></i><span class="d-none d-sm-inline"> Accounts</span></a>
+        <a class="nav-item nav-link" href="#" routerLink="/transactions" routerLinkActive="active"><i class="fas fa-list-ul"></i><span class="d-none d-sm-inline"> Transactions</span></a>
       </div>
     </div>
   </nav>
