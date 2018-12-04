9934728a202199ae3c8a630b541b597c8e6aa53c
Dmitry Ulitin <dmitry.ulitin@gmail.com>
2018-12-05 00:34:43 +0300

    fix layout

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -8,11 +8,9 @@ import { Router } from '@angular/router';
   <nav class="navbar navbar-light bg-light fixed-top">
     <div class="container">
       <span class="navbar-brand mb-0 h1">Swarmer</span>
-      <div class="d-flex flex-row">
         <a class="nav-item nav-link" href="#" routerLink="/accounts" routerLinkActive="active"><i class="fas fa-database"></i><span class="d-none d-sm-inline"> Accounts</span></a>
-        <a class="nav-item nav-link" href="#" routerLink="/transactions" routerLinkActive="active"><i class="fas fa-list-ul"></i><span class="d-none d-sm-inline"> Transactions</span></a>
-        <a class="nav-item nav-link" href="#" (click)="logout(); false" *ngIf="isAthorized">logout</a>
-      </div>
+        <a class="nav-item nav-link mr-auto" href="#" routerLink="/transactions" routerLinkActive="active"><i class="fas fa-list-ul"></i><span class="d-none d-sm-inline"> Transactions</span></a>
+        <a class="nav-item nav-link" href="#" (click)="logout(); false" *ngIf="isAthorized"><i class="fas fa-sign-out-alt"></i><span class="d-none d-sm-inline"> Logout</span></a>
     </div>
   </nav>
   <div class="container h-100">
