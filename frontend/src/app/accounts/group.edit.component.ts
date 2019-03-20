8b452d0d8f7d7cec135ff8265c367a95216033fc
Dmitry Ulitin <dmitry.ulitin@nexign-systems.com>
2019-03-20 15:41:57 +0300

    layout

--- frontend/src/app/accounts/group.edit.component.ts
+++ frontend/src/app/accounts/group.edit.component.ts
@@ -14,16 +14,20 @@ import { State } from '../app.reducers'
 export class GroupEditComponent implements OnInit {
   form: FormGroup;
   accounts: FormArray;
+  permissions: FormArray;
   constructor(private store: Store<State>, private route: ActivatedRoute, private location: Location, private fb: FormBuilder) {}
 
   ngOnInit() {
     this.accounts = this.fb.array([]);
+    this.permissions = this.fb.array([]);
     this.form = this.fb.group({
       id: [],
       name: ['', Validators.required],
       hidden: [false],
       inbalance: [true],
-      accounts: this.accounts
+      accounts: this.accounts,
+      permissions: this.permissions,
+      username:['']
     });
     this.addAccount();
     this.store.select('groups', 'sgrp').pipe(filter(g => g != null)).forEach(g => {
@@ -31,6 +35,9 @@ export class GroupEditComponent implements OnInit {
       this.accounts = this.fb.array([]);
       this.form.setControl('accounts', this.accounts);
       g.accounts.forEach(a => this.accounts.push(this.fb.group({id: a.id, start_balance: a.start_balance, currency: [{value:a.currency, disabled: a.start_balance != a.balance}], name: a.name, deleted: a.deleted})));
+      this.permissions = this.fb.array([]);
+      this.form.setControl('permissions', this.permissions);
+      g.permissions.forEach(p => this.permissions.push(this.fb.group({id: p.user.id, email: p.user.email, admin: p.admin, write: [{value:p.write, disabled: p.admin}], read: [{value: true, disabled: true}]})));
     });
     this.route.params.forEach(p => this.store.dispatch({ type: '[group] query id', payload: p['id']}));
   }
@@ -45,6 +52,17 @@ export class GroupEditComponent implements OnInit {
 
   delete(item) {
     item.get('deleted').setValue(true);
+  }
+
+  check(item) {
+    if (item.get('admin').value) {
+      item.get('write').disable();
+      item.get('read').disable();
+      item.get('write').setValue(true);
+    }
+    else {
+      item.get('write').enable();
+    }
   }
 
   addAccount() {
